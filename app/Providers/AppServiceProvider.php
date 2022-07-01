c3d0010c535fce398e6924a585c535eff742d93d
William Trevisan <williamtrev@hotmail.com>
2022-07-01 19:01:16 -0300

    feat: regiter singletons for all repositories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,25 @@
 
 namespace App\Providers;
 
+use App\Repositories\Book\Eloquent\AuthorEloquentRepository;
+use App\Repositories\Book\Eloquent\BookEloquentRepository;
+use App\Repositories\Citizen\Eloquent\CitizenEloquentRepository;
+use App\Repositories\Library\Eloquent\LibraryEloquentRepository;
+use Core\Domain\Book\Repository\AuthorRepositoryInterface;
+use Core\Domain\Book\Repository\BookRepositoryInterface;
+use Core\Domain\Citizen\Repository\CitizenRepositoryInterface;
+use Core\Domain\Library\Repository\LibraryRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public $singletons = [
+        CitizenRepositoryInterface::class => CitizenEloquentRepository::class,
+        LibraryRepositoryInterface::class => LibraryEloquentRepository::class,
+        AuthorRepositoryInterface::class => AuthorEloquentRepository::class,
+        BookRepositoryInterface::class => BookEloquentRepository::class,
+    ];
+
     /**
      * Register any application services.
      *
